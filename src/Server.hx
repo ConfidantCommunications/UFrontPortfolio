e72bfa233dae1bda1af7b961abaa769bbd477ec6
Allan Dowdeswell <secrets@confidant.ca>
2017-08-09 13:58:02 -0600

    Compiled using Haxe 3.4.2, including PHP magic in Server.hx to fix missing DefaultResolver class.

--- src/Server.hx
+++ src/Server.hx
@@ -6,11 +6,15 @@ import ufront.view.UFViewEngine;
 //import erazor.*;
 using ufront.MVC;
 
+
 class Server
 {
 	static function main() 
 	{
-		
+		//lines below resolve a problem with not finding the DefaultResolver class, a new problem since upgrading to Haxe 3.4.2
+		#if php
+		untyped __php__("require 'haxe/_Unserializer/DefaultResolver.class.php'; ");
+		#end
 		//UFViewEngine.engines.add(new ErazorViewEngine()); // <-- Adding Erazor view engine
 
 		var ufrontApp = new UfrontApplication({
